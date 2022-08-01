95e00ebf1c1d49428d5741db7d8e9e21447f8915
Sub <thomas06gjr@gmail.com>
2022-07-31 23:22:37 -0700

    added in sending of titles

--- src/main/java/me/sub/hcfactions/Commands/Admin/LunarCommand.java
+++ src/main/java/me/sub/hcfactions/Commands/Admin/LunarCommand.java
@@ -1,6 +1,8 @@
 package me.sub.hcfactions.Commands.Admin;
 
+import com.lunarclient.bukkitapi.nethandler.client.LCPacketTitle;
 import com.lunarclient.bukkitapi.nethandler.shared.LCPacketEmoteBroadcast;
+import com.lunarclient.bukkitapi.title.TitleType;
 import me.sub.hcfactions.Files.Locale.Locale;
 import me.sub.hcfactions.Main.Main;
 import me.sub.hcfactions.Utils.Color.C;
@@ -11,6 +13,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LunarCommand implements CommandExecutor {
     @Override
@@ -31,6 +34,10 @@ public class LunarCommand implements CommandExecutor {
                     } else {
                         p.sendMessage(C.chat(Locale.get().getString("command.lunar.none")));
                     }
+                } else if (args[0].equalsIgnoreCase("sendtitle")) {
+                    p.sendMessage(C.chat(Locale.get().getString("command.lunar.sendtitle-usage").replace("%alias%", label)));
+                } else if (args[0].equalsIgnoreCase("sendsubtitle")) {
+                    p.sendMessage(C.chat(Locale.get().getString("command.lunar.sendsubtitle-usage").replace("%alias%", label)));
                 } else {
                     p.sendMessage(C.chat(Locale.get().getString("command.lunar.usage").replace("%alias%", label)));
                 }
@@ -46,6 +53,10 @@ public class LunarCommand implements CommandExecutor {
                     } else {
                         p.sendMessage(C.chat(Locale.get().getString("command.lunar.not-player")));
                     }
+                } else if (args[0].equalsIgnoreCase("sendtitle")) {
+                    p.sendMessage(C.chat(Locale.get().getString("command.lunar.sendtitle-usage").replace("%alias%", label)));
+                } else if (args[0].equalsIgnoreCase("sendsubtitle")) {
+                    p.sendMessage(C.chat(Locale.get().getString("command.lunar.sendsubtitle-usage").replace("%alias%", label)));
                 } else {
                     p.sendMessage(C.chat(Locale.get().getString("command.lunar.usage").replace("%alias%", label)));
                 }
@@ -72,11 +83,95 @@ public class LunarCommand implements CommandExecutor {
                     } else {
                         p.sendMessage(C.chat(Locale.get().getString("command.lunar.not-player")));
                     }
+                } else if (args[0].equalsIgnoreCase("sendtitle")) {
+                    p.sendMessage(C.chat(Locale.get().getString("command.lunar.sendtitle-usage").replace("%alias%", label)));
+                } else if (args[0].equalsIgnoreCase("sendsubtitle")) {
+                    p.sendMessage(C.chat(Locale.get().getString("command.lunar.sendsubtitle-usage").replace("%alias%", label)));
                 } else {
                     p.sendMessage(C.chat(Locale.get().getString("command.lunar.usage").replace("%alias%", label)));
                 }
             } else {
-                p.sendMessage(C.chat(Locale.get().getString("command.lunar.usage").replace("%alias%", label)));
+                if (args[0].equalsIgnoreCase("sendtitle")) {
+                    Player player = Bukkit.getPlayer(args[1]);
+                    if (player != null) {
+                        try {
+                            long displayTime = Long.parseLong(args[2]);
+                            long fadeInTime = Long.parseLong(args[3]);
+                            long fadeOutTime = Long.parseLong(args[4]);
+                            String message = String.join(" ", Arrays.copyOfRange(args, 5, args.length));
+                            if (Main.getInstance().lunarClientAPI.isRunningLunarClient(player)) {
+                                LCPacketTitle packetTitle = new LCPacketTitle(TitleType.TITLE.name(), C.chat(message), displayTime, fadeInTime, fadeOutTime);
+                                Main.getInstance().lunarClientAPI.sendPacket(player, packetTitle);
+                                p.sendMessage(C.chat(Locale.get().getString("command.lunar.senttitle").replace("%player%", player.getName()).replace("%message%", message).replace("%fade-in%", String.valueOf(fadeInTime)).replace("%fade-out%", String.valueOf(fadeOutTime)).replace("%display%", String.valueOf(displayTime))));
+                            } else {
+                                p.sendMessage(C.chat(Locale.get().getString("command.lunar.cant-title").replace("%player%", player.getName())));
+                            }
+                        } catch (NumberFormatException nfe) {
+                            p.sendMessage(C.chat(Locale.get().getString("command.lunar.not-valid")));
+                        }
+                    } else {
+                        if (args[1].equalsIgnoreCase("all")) {
+                            try {
+                                long displayTime = Long.parseLong(args[2]);
+                                long fadeInTime = Long.parseLong(args[3]);
+                                long fadeOutTime = Long.parseLong(args[4]);
+                                String message = String.join(" ", Arrays.copyOfRange(args, 5, args.length));
+                                p.sendMessage(C.chat(Locale.get().getString("command.lunar.senttitle-all").replace("%message%", message).replace("%fade-in%", String.valueOf(fadeInTime)).replace("%fade-out%", String.valueOf(fadeOutTime)).replace("%display%", String.valueOf(displayTime))));
+                                for (Player d : Bukkit.getOnlinePlayers()) {
+                                    if (Main.getInstance().lunarClientAPI.isRunningLunarClient(d)) {
+                                        LCPacketTitle packetTitle = new LCPacketTitle(TitleType.TITLE.name(), C.chat(message), displayTime, fadeInTime, fadeOutTime);
+                                        Main.getInstance().lunarClientAPI.sendPacket(d, packetTitle);
+                                    }
+                                }
+                            } catch (NumberFormatException nfe) {
+                                p.sendMessage(C.chat(Locale.get().getString("command.lunar.not-valid")));
+                            }
+                        } else {
+                            p.sendMessage(C.chat(Locale.get().getString("command.lunar.not-player")));
+                        }
+                    }
+                } else if (args[0].equalsIgnoreCase("sendsubtitle")) {
+                    Player player = Bukkit.getPlayer(args[1]);
+                    if (player != null) {
+                        try {
+                            long displayTime = Long.parseLong(args[2]);
+                            long fadeInTime = Long.parseLong(args[3]);
+                            long fadeOutTime = Long.parseLong(args[4]);
+                            String message = String.join(" ", Arrays.copyOfRange(args, 5, args.length));
+                            if (Main.getInstance().lunarClientAPI.isRunningLunarClient(player)) {
+                                LCPacketTitle packetTitle = new LCPacketTitle(TitleType.SUBTITLE.name(), C.chat(message), displayTime, fadeInTime, fadeOutTime);
+                                Main.getInstance().lunarClientAPI.sendPacket(player, packetTitle);
+                                p.sendMessage(C.chat(Locale.get().getString("command.lunar.sentsubtitle").replace("%player%", player.getName()).replace("%message%", message).replace("%fade-in%", String.valueOf(fadeInTime)).replace("%fade-out%", String.valueOf(fadeOutTime)).replace("%display%", String.valueOf(displayTime))));
+                            } else {
+                                p.sendMessage(C.chat(Locale.get().getString("command.lunar.cant-subtitle").replace("%player%", player.getName())));
+                            }
+                        } catch (NumberFormatException nfe) {
+                            p.sendMessage(C.chat(Locale.get().getString("command.lunar.not-valid")));
+                        }
+                    } else {
+                        if (args[1].equalsIgnoreCase("all")) {
+                            try {
+                                long displayTime = Long.parseLong(args[2]);
+                                long fadeInTime = Long.parseLong(args[3]);
+                                long fadeOutTime = Long.parseLong(args[4]);
+                                String message = String.join(" ", Arrays.copyOfRange(args, 5, args.length));
+                                p.sendMessage(C.chat(Locale.get().getString("command.lunar.sentsubtitle-all").replace("%message%", message).replace("%fade-in%", String.valueOf(fadeInTime)).replace("%fade-out%", String.valueOf(fadeOutTime)).replace("%display%", String.valueOf(displayTime))));
+                                for (Player d : Bukkit.getOnlinePlayers()) {
+                                    if (Main.getInstance().lunarClientAPI.isRunningLunarClient(d)) {
+                                        LCPacketTitle packetTitle = new LCPacketTitle(TitleType.SUBTITLE.name(), C.chat(message), displayTime, fadeInTime, fadeOutTime);
+                                        Main.getInstance().lunarClientAPI.sendPacket(d, packetTitle);
+                                    }
+                                }
+                            } catch (NumberFormatException nfe) {
+                                p.sendMessage(C.chat(Locale.get().getString("command.lunar.not-valid")));
+                            }
+                        } else {
+                            p.sendMessage(C.chat(Locale.get().getString("command.lunar.not-player")));
+                        }
+                    }
+                } else {
+                    p.sendMessage(C.chat(Locale.get().getString("command.lunar.usage").replace("%alias%", label)));
+                }
             }
         } else {
             sender.sendMessage(C.chat(Locale.get().getString("primary.not-player")));
